3bd5b9d62c3e9d3a76dba04dda444473d695b391
bala-amypo <bala@amypo.in>
2025-12-23 05:37:49 +0000

    User commit

--- src/main/java/com/example/demo/controller/CategorizationRuleController.java
+++ src/main/java/com/example/demo/controller/CategorizationRuleController.java
@@ -1,9 +1,11 @@
 package com.example.demo.controller;
 
 import java.util.List;
+
 import org.springframework.web.bind.annotation.*;
+
 import com.example.demo.model.CategorizationRule;
-import com.example.demo.service.impl.CategorizationRuleServiceImpl;
+import com.example.demo.service.Impl.CategorizationRuleServiceImpl;
 
 @RestController
 @RequestMapping("/api/rules")
@@ -15,8 +17,8 @@ public class CategorizationRuleController {
         this.service = service;
     }
 
-    @GetMapping("/match")
-    public List<CategorizationRule> matchRules(@RequestParam String text) {
+    @GetMapping("/search")
+    public List<CategorizationRule> search(@RequestParam String text) {
         return service.getMatchingRules(text);
     }
 }
