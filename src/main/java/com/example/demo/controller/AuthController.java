4e73c443786ee7893c9f42ce13ba830afe05528f
bala-amypo <bala@amypo.in>
2025-12-23 04:46:02 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.*;
 import com.example.demo.dto.AuthRequest;
 import com.example.demo.dto.AuthResponse;
 import com.example.demo.model.User;
-
 import com.example.demo.service.UserService;
 
 @RestController
@@ -13,26 +12,22 @@ import com.example.demo.service.UserService;
 public class AuthController {
 
     private final UserService userService;
-    private final JwtUtil jwtUtil;
 
-    public AuthController(UserService userService,
-                          JwtUtil jwtUtil) {
+    public AuthController(UserService userService) {
         this.userService = userService;
-        this.jwtUtil = jwtUtil;
     }
 
     @PostMapping("/login")
     public AuthResponse login(@RequestBody AuthRequest request) {
 
         User user = userService.findByEmail(request.getEmail());
-        String token = jwtUtil.generateToken(
-                user.getId(),
-                user.getEmail(),
-                user.getRole());
 
-        return new AuthResponse(token,
+        // No JWT generation
+        return new AuthResponse(
+                null,                // token removed
                 user.getId(),
                 user.getEmail(),
-                user.getRole());
+                user.getRole()
+        );
     }
 }
