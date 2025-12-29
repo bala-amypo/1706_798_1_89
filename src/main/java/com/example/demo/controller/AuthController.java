8b3b1bd59b650b31bd83ed6f5bd4b9076bde9d1a
bala-amypo <bala@amypo.in>
2025-12-29 10:13:12 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -2,8 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.AuthRequest;
 import com.example.demo.dto.AuthResponse;
-import com.example.demo.entity.AppUser;
-import com.example.demo.service.AppUserService;
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,21 +11,21 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 public class AuthController {
     
-    private final AppUserService appUserService;
+    private final UserService UserService;
     
-    public AuthController(AppUserService appUserService) {
-        this.appUserService = appUserService;
+    public AuthController(UserService UserService) {
+        this.UserService = UserService;
     }
 
     @PostMapping("/register")
     public ResponseEntity<AppUser> register(@RequestBody AuthRequest request) {
-        AppUser user = appUserService.register(request.getEmail(), request.getPassword(), "USER");
+        AppUser user = UserService.register(request.getEmail(), request.getPassword(), "USER");
         return ResponseEntity.ok(user);
     }
 
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
-        AuthResponse response = appUserService.login(request.getEmail(), request.getPassword());
+        AuthResponse response = UserService.login(request.getEmail(), request.getPassword());
         return ResponseEntity.ok(response);
     }
 }
